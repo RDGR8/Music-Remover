db64d4f666af296c68374ffd1a0dd6aead95bcb5
RD8 <go_gamer8@hotmail.com>
2025-02-08 19:09:58 +0400

    Adding tab spaces in echos for sub-stages

--- install MR.bat
+++ install MR.bat
@@ -34,7 +34,7 @@ exit
 
 : START
 
-set "version=0.0.2"
+set "version=0.0.1"
 
 :: Check if Visual C++ is installed
 echo Checking if Visual C++ is installed
@@ -89,38 +89,34 @@ set "psCommand="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Please 
 for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "location=%%I
 cd %location%
 
-echo Downloading and extracting the app
+echo App Setup
+
+echo 	Downloading and extracting the app
 curl -L "https://github.com/RDGR8/Music-Remover/archive/refs/tags/v%version%.zip" -o MusicRemoverSourceCode.zip
 tar -xf MusicRemoverSourceCode.zip
 del MusicRemoverSourceCode.zip
 cd Music-Remover-%version%
 
-:: Download embedded python 3.11.9
-echo Downloading embedded python
-curl -LO "https://www.python.org/ftp/python/3.11.9/python-3.11.9-embed-amd64.zip"
-tar -xf python-3.11.9-embed-amd64.zip
-del python-3.11.9-embed-amd64.zip
-
-:: Download script to install python for embedded python
-curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-.\python get-pip.py
-echo Lib >> python311._pth 
-echo Lib/site-packages >> python311._pth
-echo Scripts >> python311._pth
-echo tcl >> python311._pth
 
 :: extract and install some stuff needed for the program to work
-echo extracting tkiner and ffmpeg
-tar -xf tkinter.zip
-del tkinter.zip
+echo 	Extracting ffmpeg
 tar -xf ffmpeg.zip
 del ffmpeg.zip
-.\python Scripts\pip.exe install -r requirements.txt
-.\python Scripts\pip.exe install audio-separator[cpu]==0.28.5 onnx==1.16.1 -t .\CPUseparator
-.\python Scripts\pip.exe cache purge
+
+py -3.11 -m venv .venv
+call .venv/Scripts/activate.bat
+
+echo 	Installing dependencies 
+pip install -r requirements.txt
+pip install audio-separator[cpu]==0.28.5 onnx==1.16.1 -t .venv\CPUseparator
+pip cache purge
+
+call .venv/Scripts/deactivate.bat
 
 :: Making shortcuts
-echo creating Start Menu shortcut
+echo Creating Start Menu shortcut
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Music Remover.lnk');$s.TargetPath='%cd%\MR.vbs';$s.Arguments='connect';$s.IconLocation='%cd%\icon.ico';$s.WorkingDirectory='%cd%';$s.WindowStyle=7;$s.Save()"
 echo creating a Desktop shortcut
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\Music Remover.lnk');$s.TargetPath='%cd%\MR.vbs';$s.Arguments='connect';$s.IconLocation='%cd%\icon.ico';$s.WorkingDirectory='%cd%';$s.WindowStyle=7;$s.Save()"
+echo Installation Finished
+pause
